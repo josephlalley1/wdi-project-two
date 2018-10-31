01d1afcf2cfd5e7fd4a819f9005b61f45443460a
Joseph Lalley <josephyzyr@gmail.com>
2018-10-31 14:58:42 +0000

    added more content

--- db/create-data.js
+++ db/create-data.js
@@ -10,8 +10,8 @@ User.collection.drop();
 const userData = [
   {
     profilepic: 'https://data.whicdn.com/images/263815632/large.jpg',
-    username: 'kb',
-    email: 'kodakbl@ck',
+    username: 'Administrator',
+    email: 'admin@hp',
     password: 'pass'
   }
 ];
@@ -30,6 +30,34 @@ User.create(userData).then(users => {
       coverimg: 'https://video-images.vice.com/articles/5bcf506f61347e00069d9daa/lede/1540314056277-shutterstock_582236128.jpeg?crop=1xw%3A0.7959214808787632xh%3Bcenter%2Ccenter&resize=2000%3A',
       frontPageSummary: 'The ISP, called Pocket iNet, left 73 gigabytes of extremely sensitive internet available for anyone to see.',
       content: 'In what’s becoming a bit of a trend in modern technical incompetence, yet another company has left oodles of private company data sitting openly accessible on an Amazon cloud server. According to a new report by the security researchers at UpGuard, a Washington-based ISP by the name of Pocket iNet left 73 gigabytes of essential operational data publicly exposed in a misconfigured Amazon S3 storage bucket for months.',
+      addedBy: users[0]
+    },
+    {
+      title: '10 Years of the Blockchain: What Is Bitcoin, Really?',
+      coverimg: 'https://video-images.vice.com/articles/5bd8c896595ea8000612d2d9/lede/1540935973418-GettyImages-1011245260.jpeg?crop=0.9986635797240575xw%3A1xh%3Bcenter%2Ccenter&resize=2000%3A',
+      frontPageSummary: 'It’s been 10 years since someone going by the pseudonym Satoshi Nakamoto posted the white paper describing Bitcoin to a cryptography mailing list, and we are all very tired.',
+      content: 'Whether you like it or not, the original cryptocurrency is a part of most of our lives now. If you’re not an ultra-libertarian net warrior yourself, then you’ve probably been exposed to someone babbling about blockchains at a party, or at a coffee shop, or on the news, or, god help you, around the dinner table. Even people who’ve never heard of Bitcoin can be affected by it—say, in the towns where the industry’s intense energy needs have suddenly become everybody’s problem.',
+      addedBy: users[0]
+    },
+    {
+      title: 'The SIM Hijackers',
+      coverimg: 'https://video-images.vice.com/articles/5b324d2cf3af8d00076a6d11/lede/1530023267166-hackers_motherboard5.jpeg?crop=0.9977777777777778xw%3A1xh%3Bcenter%2Ccenter&resize=2000%3A',
+      frontPageSummary: 'Meet the hackers who flip seized Instagram handles and cryptocurrency in a shady, buzzing underground market for stolen accounts and usernames. Their victims’ weakness? Phone numbers.',
+      content: 'It seemed like any other warm September night in the suburbs of Salt Lake City. Rachel Ostlund had just put her kids to bed and was getting ready to go to sleep herself. She was texting with her sister when, unexpectedly, her cell phone lost service. The last message Rachel received was from T-Mobile, her carrier. The SIM card for her phone number, the message read, had been “updated.”',
+      addedBy: users[0]
+    },
+    {
+      title: 'How ‘Mr. Hashtag’ Helped Saudi Arabia Spy on Dissidents',
+      coverimg: 'https://video-images.vice.com/articles/5bd742a90f03a20006492022/lede/1540835851715-Screen-Shot-2018-10-29-at-15643-PM.png?crop=1xw%3A0.8356481481481481xh%3Bcenter%2Ccenter&resize=2000%3A',
+      frontPageSummary: 'Earlier this month, security researchers revealed that the Saudi Arabian government tried to hack a prominent Saudi dissident and human rights worker who lives in Canada.',
+      content: 'This came just a few weeks after Amnesty International accused the country of using sophisticated spyware to hack one of its researchers. Then, the New York Times revealed that the Saudis have turned a Twitter employee into a spy who helped them keep tabs on digital rights activists by accessing their accounts and private messages.',
+      addedBy: users[0]
+    },
+    {
+      title: 'Russian Malware Was Apparently Used in an Attempt to Sabotage a Saudi Petrol Plant',
+      coverimg: 'https://video-images.vice.com/articles/5bcf43295016250006b89f40/lede/1540310259299-shutterstock_624964346.jpeg?crop=0.9985185185185186xw%3A1xh%3Bcenter%2Ccenter&resize=2000%3A',
+      frontPageSummary: 'The hackers who created a malware that is believed to have sabotaged a Saudi Arabia petrochemical plant last year were working for the Russian government, according to a new report by a cybersecurity firm.',
+      content: 'Earlier this year, researchers revealed a new type of malware that reportedly could have made a Saudi plant explode. They called the malware Triton or Trisis. Now, security firm FireEye revealed in a blog post that the Russian government was behind Triton, after a German daily broke the news of the attribution.',
       addedBy: users[0]
     }
   ]).then(() => mongoose.connection.close());
