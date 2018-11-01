2c724f5abab31d22656901bcd53c34f9b8acb19e
Joseph Lalley <josephyzyr@gmail.com>
2018-11-01 09:40:28 +0000

    Set up for deployment to Heroku

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
-const dbUri = 'mongodb://localhost/honeypot';
-const port = 4000;
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/honeypot';
+const port = process.env.PORT || 4000;
 
 module.exports = {
   dbUri: dbUri,
